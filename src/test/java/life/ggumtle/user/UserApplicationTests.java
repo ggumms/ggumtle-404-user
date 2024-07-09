5e8583a242fba641226bc02872188628410564c9
신창엽 <schy2504@naver.com>
2024-07-10 08:19:20 +0900

    Setting #1: Basic spring framework
    - Setting basic framework with no dependencies
    - .gitignore file update

---
+++ src/test/java/life/ggumtle/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package life.ggumtle.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
